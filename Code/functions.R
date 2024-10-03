b547fe73773e16f4afdbf0f533668d4889638be4
Iri-Lrnr <37641692+IriLrnr@users.noreply.github.com>
2024-10-03 13:31:14 -0300

    final changes
    stopped after this

--- Code/functions.R
+++ Code/functions.R
@@ -1,7 +1,7 @@
 source("libraries.R")
 
 # edit data for analysis
-edit_shp2 <- function (data) {
+edit_shp <- function (data) {
   
   # EVI Indexes on the sample
   data$r100_total <-(data$r100_C + data$r100_B + data$r100_A + data$r100_NODAT)
@@ -41,9 +41,12 @@ edit_shp2 <- function (data) {
       SGVIproximal = if_else(EVI_class == "C" & r100_total - 1 != 0, (r100_C - 1) / (r100_total - 1), SGVIproximal),
       SUVIproximal = if_else(r100_total - 1 == 0, NA_real_, r100_A / (r100_total - 1)),
       SUVIproximal = if_else(EVI_class == "A" & r100_total - 1 != 0, (r100_A - 1) / (r100_total - 1), SUVIproximal),
-      SBVIproximal = if_else(r100_total - 1 == 0, NA_real_, (1 + (r100_C - r100_A) / (r100_total - 1))/2),
-      SBVIproximal = if_else(EVI_class == "C" & r100_total - 1 != 0, (1 + (r100_C - r100_A - 1) / (r100_total - 1))/2, SBVIproximal),
-      SBVIproximal = if_else(EVI_class == "A" & r100_total - 1 != 0, (1 + (r100_C - r100_A + 1) / (r100_total - 1))/2, SBVIproximal)
+      #SBVIproximal = if_else(r100_total - 1 == 0, NA_real_, (1 + ((r100_C - r100_A) / (r100_total - 1)))/2),
+      #SBVIproximal = if_else(EVI_class == "C" & r100_total - 1 != 0, (1 + ((r100_C - r100_A - 1) / (r100_total - 1)))/2, SBVIproximal),
+      #SBVIproximal = if_else(EVI_class == "A" & r100_total - 1 != 0, (1 + ((r100_C - r100_A + 1) / (r100_total - 1)))/2, SBVIproximal)
+      SBVIproximal = if_else(r100_total - 1 == 0, NA_real_, (r100_C - r100_A) / (r100_total - 1)),
+      SBVIproximal = if_else(EVI_class == "C" & r100_total - 1 != 0, (r100_C - r100_A - 1) / (r100_total - 1), SBVIproximal),
+      SBVIproximal = if_else(EVI_class == "A" & r100_total - 1 != 0, (r100_C - r100_A + 1) / (r100_total - 1), SBVIproximal)
     )
   
   # Calculate SEVIproximal
@@ -60,7 +63,8 @@ edit_shp2 <- function (data) {
       SVIdistal = if_else((r500_total - r100_total) == 0, NA_real_, (r500_B - r100_B) / (r500_total - r100_total)),
       SGVIdistal = if_else((r500_total - r100_total) == 0, NA_real_, (r500_C - r100_C) / (r500_total - r100_total)),
       SUVIdistal = if_else((r500_total - r100_total) == 0, NA_real_, (r500_A - r100_A) / (r500_total - r100_total)),
-      SBVIdistal = if_else((r500_total - r100_total) == 0, NA_real_, (1 + (r500_C - r100_C) - (r500_A - r100_A)/ (r500_total - r100_total))/2)
+      #SBVIdistal = if_else((r500_total - r100_total) == 0, NA_real_, (1 + r500_SGVI - r500_SUVI)/2)
+      SBVIdistal = if_else((r500_total - r100_total) == 0, NA_real_, r500_SGVI - r500_SUVI)
     )
   
   # Calculate SEVIdistal
@@ -69,10 +73,8 @@ edit_shp2 <- function (data) {
       SEVIdistal = if_else(((r500_C - r100_C) + (r500_A - r100_A)) == 0, NA_real_, ((r500_C - r100_C) - (r500_A - r100_A)) / ((r500_C - r100_C) + (r500_A - r100_A)))
     )
   
-  #data$SEVIproximal <- (1+data$SEVIproximal)/2
-  #data$SEVIproximal <- (1+data$SEVIproximal)/2
-  
   clean_data <- data
+  clean_data$pop <- clean_data$pop * 0.09
   centroids <- st_centroid(clean_data)
   # Add centroid coordinates as new columns
   clean_data$centx <- st_coordinates(centroids)[, 1]
@@ -92,141 +94,31 @@ normalize <- function(x) {
   return((x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE)))
 }
 
-calculate_surrounding_old <- function(nb_list, data, var_name, l) {
-  data <- as.data.frame(data)
-  index <- sapply(1:length(nb_list), function(i) {
-    neighbors <- nb_list[[i]]
-    sum_var <- sum(data[neighbors, var_name], na.rm = TRUE)
-    sum_all <- sum(data[neighbors, "A"], na.rm = TRUE) +
-      sum(data[neighbors, "B"], na.rm = TRUE) +
-      sum(data[neighbors, "C"], na.rm = TRUE) +
-      sum(data[neighbors, "EVI_nodata"])
-    if (sum_all == 0) {
-      return(NA)
-    } else {
-      return(sum_var / sum_all)
-    }
-  })
-  return(index)
-}
-
-calculate_surrounding <- function(nb_list, data, var_name) {
-  data <- as.data.frame(data)
-  index <- sapply(1:length(nb_list), function(i) {
-    neighbors <- nb_list[[i]]
-    total_neighbors <- length(neighbors)
-    
-    if (total_neighbors == 0) {
-      return(NA)
-    } else {
-      count_var <- sum(data[neighbors, "EVI_class"] == var_name, na.rm = TRUE)
-      proportion_var <- count_var / l*l
-      return(proportion_var)
-    }
-  })
-  return(index)
-}
-
-calculate_surrounding_dif_old <- function(nb_list, data) {
-  data <- as.data.frame(data)
-  index <- sapply(1:length(nb_list), function(i) {
-    neighbors <- nb_list[[i]]
-    sum_C <- sum(data[neighbors, "C"], na.rm = TRUE)
-    sum_A <- sum(data[neighbors, "A"], na.rm = TRUE)
-    if (sum_A + sum_C == 0) {
-      return(NA)
-    } else {
-      return((sum_C-sum_A)/(sum_C+sum_A))
-    }
-  })
-  return(index)
-}
-
-calculate_surrounding_dif <- function(nb_list, data, var_name) {
-  data <- as.data.frame(data)
-  index <- sapply(1:length(nb_list), function(i) {
-    neighbors <- nb_list[[i]]
-    total_neighbors <- length(neighbors)
-    
-    if (total_neighbors == 0) {
-      return(NA)
-    } else {
-      count_A <- sum(data[neighbors, "EVI_class"] == "A", na.rm = TRUE)
-      count_C <- sum(data[neighbors, "EVI_class"] == "C", na.rm = TRUE)
-      return((count_C-count_A)/(count_C+count_A))
-    }
-  })
-  return(index)
-}
-
-# edit data for analysis
-edit_shp <- function (data) {
-  data$prop_veg <- (data$PV_1 + data$PV_2 + data$PV_3)/(data$PV_1 + data$PV_2 + data$PV_3 + data$PV_NODATA)
-  
-  # EVI Indexes on the sample
-  data$EVI_nodata <- (data$EVI_NODATA)/(data$C + data$B + data$A + data$EVI_NODATA)
-  data$SVI <- (data$B)/(data$C + data$B + data$A + data$EVI_NODATA)
-  data$SUVI <- (data$A)/(data$C + data$B + data$A + data$EVI_NODATA)
-  data$SGVI <- (data$C)/(data$C + data$B + data$A + data$EVI_NODATA)
-  data$SEVI <- (data$C - data$A)/(data$C + data$A)
-  #data$SBVI <- abs(data$C - data$A)/(data$C + data$B + data$A + data$EVI_NODATA)
-  
-  #data$pop_d <- data$pop/data$ocup_area
-  
-  data$SBmean <- data$SBsum/data$SBcount
-
-  clean_data <- data
-  #clean_data <- data[!is.na(data$EVI_mean),]
-  clean_data <- data[data$EVI_nodata < 0.3,]
-  
-  centroids <- st_centroid(clean_data)
-  # Add centroid coordinates as new columns
-  clean_data$centx <- st_coordinates(centroids)[, 1]
-  clean_data$centy <- st_coordinates(centroids)[, 2]
-  
-  abdata <- clean_data[!is.na(clean_data$SBmean),]
-  
-  clean_data <- clean_data %>%
-    mutate(CR = DT,
-           AB = SBmean,
-           pop = pop)
-  
-  clean_data <- clean_data %>%
-    mutate(CRn = normalize(clean_data$DT),
-           ABn = normalize(clean_data$SBmean),
-           popn = normalize(clean_data$pop))
-  
-  #clean_data <- clean_data[,c("id", "CR", "AB", "pop", "CRn", "ABn", "popn", "SVI", "SGVI", "SUVI", "SEVI", "prop_veg", "centx", "centy")]
-  
-  class(clean_data)
-  
-  return(clean_data)
-}
-
 plot_map <- function(data, res, var) {
   data$r <- as.data.frame(data)[,c(paste(res))]
   ggplot(data = data) +
     geom_sf(aes(fill = r), color = NA) +
-    scale_fill_viridis_c() +
+    scale_fill_viridis_c(option = "turbo", limits = c(-1, 1)) +
     custom_theme +
     theme(legend.position = "none",
           plot.title = element_text(size = 10),
           axis.text = element_blank()) +
-    labs(title = var)
+    labs(title = "")
 }
 
 plot_tern <- function(data, var, color= "turbo") {
   color = "turbo"
   data$var <- (as.data.frame(data)[,c(paste(var))])
   ggtern(data, aes(x = SGVIproximal, y = SVIproximal, z = SUVIproximal, color = (var))) +
-    geom_point(size = 1) +
+    geom_point(size = 0.5, alpha = 0.5) +
     scale_color_viridis_c(option = color, limits = c(-1,1)) +
+    theme_hidetitles() +
     theme_hidelabels() +
     theme_hideticks() +
     theme(
       legend.position = "none",
-    ) +
-    labs(x = "SGVI", y = "SVI", z = "SUVI")
+    ) 
+    # + labs(x = "SGVI", y = "SVI", z = "SUVI")
 }
 
 plot_tern2 <- function(data, var, color = "turbo") {
@@ -235,12 +127,13 @@ plot_tern2 <- function(data, var, color = "turbo") {
     ggtern(data, aes(x = SGVIdistal, y = SVIdistal, z = SUVIdistal, color = (var))) +
       geom_point(size = 1) +
       scale_color_viridis_c(option = color, limits = c(-1,1)) +
+      theme_hidetitles() +
       theme_hidelabels() +
       theme_hideticks() +
       theme(
         legend.position = "none",
-      ) +
-      labs(x = "SGVI", y = "SVI", z = "SUVI")
+      ) 
+      #+ labs(x = "SGVI", y = "SVI", z = "SUVI") 
 }
 
 # make a bi plot map to see overlaps - not using much
@@ -282,15 +175,25 @@ plot_bi_map <- function(d, res, pred, pal = "PinkGrn") {
   #print(table(as.factor(sdb$bi_class)))
 }
 
-add_centroids <- function (data) {
-  # Exclude polygons with no neighbours from your data
-  clean_data <- data
+plot_dummy_legend <- function() {
+  legend_plot <- ggplot(data.frame(x = 0, y = 0, z = seq(0, 1, length.out = 100)), aes(x, y, fill = z)) +
+    geom_tile(NULL) +
+    scale_fill_viridis_c(
+      guide = guide_colorbar(
+        direction = "horizontal",
+        title = NULL,
+        label.position = "bottom",
+        barwidth = 20,
+        barheight = 1)) +
+    theme_void() +
+    theme(
+      legend.position = "bottom",
+      legend.title = element_blank(),
+      legend.text = element_text(size = 10))
   
-  centroids <- st_centroid(clean_data)
-  # Add centroid coordinates as new columns
-  clean_data$centx <- st_coordinates(centroids)[, 1]
-  clean_data$centy <- st_coordinates(centroids)[, 2]
+  # Extract the legend as a grob
+  shared_legend <- cowplot::get_plot_component(legend_plot, 'guide-box-bottom', return_all = TRUE)
   
-  return(clean_data)
+  legend_grob <- cowplot::ggdraw(shared_legend)
+  return(legend_grob)
 }
-
